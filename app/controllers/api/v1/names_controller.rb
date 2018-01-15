7be6cb31cf48935b5270c74232d63c74f1243b06
jayphodges <jayphodges@gmail.com>
2018-01-15 15:14:01 -0700

    Refactored code out of controller

--- app/controllers/api/v1/names_controller.rb
+++ app/controllers/api/v1/names_controller.rb
@@ -2,27 +2,16 @@ class Api::V1::NamesController < ApplicationController
   before_action :set_name
 
   def show
+    render json: Name.chart_return(@name)
+  end
+
+  def index
     render json: @combined
   end
 
   private
     def set_name
-      @name = Name.where(name: params[:name].capitalize, gender: 'M')
-      .order('year').pluck(:year, :count)
-      @fem = Name.where(name: params[:name].capitalize, gender: 'F')
-      .order('year').pluck(:year, :count)
-      @combined = {male: format(@name), female: format(@fem)}
-    end
-
-    def format(data)
-      hash = {2000 => 0, 2001 => 0, 2002 => 0, 2003 => 0,
-        2004 => 0, 2005 => 0, 2006 => 0, 2007 => 0,
-        2007 => 0, 2008 => 0, 2009 => 0, 2010 => 0,
-        2011 => 0, 2012 => 0, 2013 => 0, 2014 => 0}
-      data.each do |something|
-        hash[something[0]] = something[1]
-      end
-      return hash.values
+      @name = params[:name].capitalize
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/name/:name', to: 'names#show'
+      get '/name/year/:year', to: 'names#index'
       get '/year/:year', to: 'years#show'
     end
   end
