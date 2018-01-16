11680c1d3fe5bed500c866a72ec4458b9d947233
jayphodges <jayphodges@gmail.com>
2018-01-15 18:03:45 -0700

    Adjusted seed file to full sized state database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ CSV.foreach('lib/seeds/NationalNames2000.csv', headers: true) do |row|
 end
 puts "Imported #{Name.count} in #{(Time.now - time1).round(2)}"
 time2 = Time.now
-CSV.foreach('lib/seeds/StateNamesAK2000.csv', headers: true) do |row|
+CSV.foreach('lib/seeds/StateNames2000.csv', headers: true) do |row|
   State.create(row.to_h)
 end
 puts "Imported #{State.count} in #{(Time.now - time2).round(2)}"
