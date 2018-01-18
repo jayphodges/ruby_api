79ef6e270409657a1b0eae54c0e4830bd3a0fdaa
jayphodges <jayphodges@gmail.com>
2018-01-18 10:56:31 -0700

    Modified seed to test deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 require 'csv'
 time1 = Time.now
-CSV.foreach('lib/seeds/NationalNames2000.csv', headers: true) do |row|
+CSV.foreach('lib/seeds/test.csv', headers: true) do |row|
   Name.create(row.to_h)
 end
 puts "Imported #{Name.count} in #{(Time.now - time1).round(2)}"
 time2 = Time.now
-CSV.foreach('lib/seeds/StateNames2000.csv', headers: true) do |row|
+CSV.foreach('lib/seeds/StateNamesAK2000.csv', headers: true) do |row|
   State.create(row.to_h)
 end
 puts "Imported #{State.count} in #{(Time.now - time2).round(2)}"
